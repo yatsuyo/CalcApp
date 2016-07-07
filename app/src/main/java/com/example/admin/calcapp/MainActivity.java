0a8232a39d04d9cce2e03d7f43f0c076a4dd8feb
yatsuyo <hungry_soul_0122@yahoo.co.jp>
2016-07-07 21:07:40 +0900

    Lesson4_課題_Calc

---
+++ app/src/main/java/com/example/admin/calcapp/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.admin.calcapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra("VALUE1", 10);
+        intent.putExtra("VALUE2", 20);
+        startActivity(intent);
+    }
+}
